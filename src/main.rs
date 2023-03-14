c4c4cd174a442c8e903633df5d8ebd2d8c53624a
M-Farag <mina.farag@icloud.com>
2023-03-14 12:15:41 +0200

    UPD Change the function to accept a generic abstracted list

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use std::io::Error;
 
 fn main() {
-    let mut numbers: Vec<i32> =  vec![1,2,50,5,4,30,90,123,2];
+    let numbers: Vec<i32> =  vec![1,2,50,5,4,30,90,123,2];
     match largest(&numbers) {
         Ok(num) => println!("Max number is: {}",num),
         Err(_) => println!("Err")
     }
 
+    let numbers: Vec<f64> =  vec![1.90,1.675,1.5463,1.8604,1.423,1.30,1.901,1.123,1.2];
+    match largest(&numbers) {
+        Ok(num) => println!("Max number is: {}",num),
+        Err(_) => println!("Err")
+    }
+
+
+
 }
 
-
-fn largest(list_of_numbers:&Vec<i32>)-> Result<&i32,&str>
+// Generic
+fn largest<T: std::cmp::PartialOrd>(list_of_numbers:&Vec<T>)-> Result<&T,&str>
 {
     let mut max = list_of_numbers.get(0).ok_or("err")?;
 
